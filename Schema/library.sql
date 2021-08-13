6eacda03528c0b07aeb3b3315f74fc65698a2a09
Federico Palmieri <fedepalmie95@gmail.com>
2021-08-13 13:14:54 -0300

    changes using inner join in MySQL

--- Schema/library.sql
+++ Schema/library.sql
@@ -5,12 +5,24 @@ create table books (
 		id_book int not null auto_increment,
         book_name varchar(50),
         isbn13 varchar(17) UNIQUE,
-        author varchar(50),
-        constraint pk_books primary key(id_book)
+        id_author int,
+        constraint pk_books primary key(id_book),
+        constraint fk_books_author foreign key(id_author) references authors(id_author) ON DELETE CASCADE
+) ;
+
+create table authors (
+		id_author int not null auto_increment,
+        author_name varchar(50),
+        nationality varchar(50),
+        constraint pk_authors primary key(id_author)
 ) ;
 
 
-SELECT * FROM books;
-SELECT book_name, isbn13, author FROM books WHERE id_book = 1;
-INSERT INTO books (book_name, isbn13, author) VALUES ('Los ojos del perro siberiano', '978-987-545-706-5', 'Antonio Santa Ana');
+SELECT id_book, book_name, isbn13, a.id_author, a.author_name, a.nationality FROM books as b INNER JOIN authors as a ON b.id_author = a.id_author WHERE id_book = 1;
+
+SELECT book_name, isbn13, id_author FROM books WHERE id_book = 1;
+INSERT INTO authors (author_name, nationality) VALUES ('Antonio Santa Ana', 'Argentina');
+INSERT INTO books (book_name, isbn13, id_author) VALUES ('Los ojos del perro siberiano', '978-987-545-706-4', '2');
 DELETE FROM books WHERE id_book = 1 LIMIT 1;
+select * from authors
+drop table books;
--- index.js
+++ index.js
@@ -9,13 +9,24 @@ app.use(express.json());
 const connection = dbConnection();
 
 app.get('/books', (req, res) => {
-  connection.query('SELECT * FROM books', (err, result) => {
+  connection.query('SELECT id_book, book_name, isbn13, a.id_author, a.author_name, a.nationality FROM books as b INNER JOIN authors as a ON b.id_author = a.id_author', (err, result) => {
     if (err) {
       res.status(500).json({ message: 'database error' });
     } else if (!result.length) {
       res.status(404).json({ message: 'no books found' });
     } else {
-      res.status(200).json(result);
+      const data = [];
+      result.forEach((book) => data.push({
+        id_book: book.id_book,
+        book_name: book.book_name,
+        isbn13: book.isbn13,
+        author: {
+          id_author: book.id_author,
+          author_name: book.author_name,
+          nationality: book.nationality,
+        },
+      }));
+      res.status(200).json(data);
     }
   });
 });
@@ -23,21 +34,32 @@ app.get('/books', (req, res) => {
 app.get('/book/:id', (req, res) => {
   const { id } = req.params;
 
-  connection.query(`SELECT book_name, isbn13, author FROM books WHERE id_book = ${id} LIMIT 1`, (err, result) => {
+  connection.query(`SELECT id_book, book_name, isbn13, a.id_author, a.author_name, a.nationality FROM books as b INNER JOIN authors as a ON b.id_author = a.id_author WHERE id_book = ${id} LIMIT 1`, (err, result) => {
     if (err) {
       res.status(500).json({ message: 'database error' });
     } else if (!result.length) {
       res.status(404).json({ message: 'The requested book was not found' });
     } else {
-      res.status(200).json(result);
+      const data = [];
+      result.forEach((book) => data.push({
+        id_book: book.id_book,
+        book_name: book.book_name,
+        isbn13: book.isbn13,
+        author: {
+          id_author: book.id_author,
+          author_name: book.author_name,
+          nationality: book.nationality,
+        },
+      }));
+      res.status(200).json(data);
     }
   });
 });
 
 app.post('/books', (req, res) => {
-  const { bookName, isbn13, author } = req.body;
+  const { bookName, isbn13, authorId } = req.body;
 
-  connection.query(`INSERT INTO books (book_name, isbn13, author) VALUES ('${bookName}', '${isbn13}', '${author}')`, (err) => {
+  connection.query(`INSERT INTO books (book_name, isbn13, id_author) VALUES ('${bookName}', '${isbn13}', '${authorId}')`, (err) => {
     if (err) {
       res.status(500).json({ message: 'database error' });
       throw err;
@@ -49,9 +71,9 @@ app.post('/books', (req, res) => {
 
 app.put('/books/:id', (req, res) => {
   const { id } = req.params;
-  const { bookName, isbn13, author } = req.body;
+  const { bookName, isbn13, authorId } = req.body;
 
-  connection.query(`UPDATE books SET book_name = '${bookName}', isbn13 = '${isbn13}', author = '${author}' WHERE id_book = ${id} LIMIT 1`, (err, result) => {
+  connection.query(`UPDATE books SET book_name = '${bookName}', isbn13 = '${isbn13}', id_uthor = '${authorId}' WHERE id_book = ${id} LIMIT 1`, (err, result) => {
     if (err) {
       res.status(500).json({ message: 'database error' });
     } else if (!result.affectedRows) {
